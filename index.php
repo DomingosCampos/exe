00a72c934f8ccae0421a8445c636fa5750056141
Domingos Castro <bilgcastro71@gmail.com>
2020-03-29 14:09:02 -1100

    Teste SASS

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testando</title>
+	<link rel="stylesheet" href="recursos/node_modules/bootstrap/servicos/bootstrap.css">
+</head>
+<body>
+
+
+<div class="container">
+	<h1 class="bg-success card-title" style="color:white;">Testando o bootstrap4 na minha maquina</h1>
+	<button type="button" class="btn btn-success">Workspace</button>
+</div>
+
+
+
+	<script src="recursos/node_modules/jquery/jquery.js"></script>
+	<script src="recursos/node_modules/popper.js/dist/umd/popper.js"></script>
+	<script src="recursos/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
